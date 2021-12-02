46916f39917e0dd510acdd44decf0dfb0d55a44c
@thiankiann <thiankiann@gmail.com>
2021-12-02 19:33:01 +0000

    Exc. 1 → create endpoint which get from a user his name (as @RequestParam )and will give it back but converted to upper case

--- src/main/java/com/example/demo/controller/MyFirstRestController.java
+++ src/main/java/com/example/demo/controller/MyFirstRestController.java
@@ -4,6 +4,7 @@ import com.example.demo.service.MyService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,5 +25,16 @@ public class MyFirstRestController {
         logger.debug("is it visible???");
         return "Hello RestController";
     }
+/**
+ * Exc. 1
+ *  → create endpoint which get from a user his name (as @RequestParam )and will give it back but converted to upper case
+ * => zrobimy to w taki sposob, ze jesli uzytkownik w adresie podal parametr myName to żeby to imie bylo automatycznie wstawiane do argumentu w naszej metodce
+ * (tzn. Wpisujac http://localhost:8080/convert-name?myName=Mariusz → otrzymamy MARIUSZ )
+ */
+@GetMapping("/convert-name")
+    public String convertNameForUpperCase(@RequestParam("myName") String name) {
 
+        String result = name.toUpperCase();
+        return result;
+    }
 }
